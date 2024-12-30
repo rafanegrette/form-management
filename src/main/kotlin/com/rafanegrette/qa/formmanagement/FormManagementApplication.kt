7d21e02d6e837d7d4a7a808307240381bf9c801a
Rafael Negrette <rafaelnegretteamaya@outlook.com>
2024-12-30 18:17:09 -0500

    Add missing files to the project

---
+++ src/main/kotlin/com/rafanegrette/qa/formmanagement/FormManagementApplication.kt
@@ -0,0 +1,11 @@
+package com.rafanegrette.qa.formmanagement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FormManagementApplication
+
+fun main(args: Array<String>) {
+	runApplication<FormManagementApplication>(*args)
+}
---
+++ src/test/kotlin/com/rafanegrette/qa/formmanagement/FormManagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rafanegrette.qa.formmanagement
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FormManagementApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
